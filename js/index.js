912f36f45c48b70bd79531ce868d48b99fba070a
Bohdan Chornyi <wan-xruner@mail.ru>
2017-01-26 22:27:32 +0000

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-document.write('Hello world')
+var userName = prompt('Enter your name');
+var greet = 'Hello, + ' userName;
+document.write(greet);
+
+var userAge = Number(prompt('Enter your age'));
+var nextAge = userAge + 1;
+
+document.write('<br>Soon you will be' + nextAge)
