50213a32310dba19f7748d371ffde0eafa9321a4
Bohdan Chornyi <wan-xruner@mail.ru>
2017-02-03 17:52:47 +0000

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-var userName = prompt('Enter your name');
-var greet = 'Hello, + ' userName;
-document.write(greet);
-
-var userAge = Number(prompt('Enter your age'));
-var nextAge = userAge + 1;
-
-document.write('<br>Soon you will be' + nextAge)
+function fib(n){
+	if (n==1||n==2) return 1;
+	else return fib(n-2)+fib(n-1);		
+	}
+	var userNum=+prompt('Введите порядковый номер числа Фибоначчи');
+alert(fib(userNum));
